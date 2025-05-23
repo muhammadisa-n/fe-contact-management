13722e102da6640f7be3cddd380fcdbc743e119c
muhammadisa-n <muhammadisa226@gmail.com>
2025-05-23 11:06:40 +0700

    create register page

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,21 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.jsx'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { BrowserRouter, Route, Routes } from "react-router";
+import Layout from "./components/Layout";
+import UserRegister from "./components/User/UserRegister";
 
-createRoot(document.getElementById('root')).render(
+createRoot(document.getElementById("root")).render(
   <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <BrowserRouter>
+      <Routes>
+        <Route element={<Layout />}>
+          <Route
+            path="/contact-management/register"
+            element={<UserRegister />}
+          />
+        </Route>
+        <Route path="/contact-management/dashboard"></Route>
+      </Routes>
+    </BrowserRouter>
+  </StrictMode>
+);
